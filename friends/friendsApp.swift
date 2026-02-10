3fc30c616d2ff36dca2141c97ef59f5150e66cd6
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-10 11:00:04 -0600

    Initial Commit

---
+++ friends/friendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendsApp.swift
+//  friends
+//
+//  Created by Alonso Acosta on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct friendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
